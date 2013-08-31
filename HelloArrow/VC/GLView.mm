55d1721307089a077e31807d8315de8a94bebc68
wneo <neo@neotekiMac.local>
2013-08-31 09:47:01 -0400

    initialition the project complite

--- HelloArrow/VC/GLView.mm
+++ HelloArrow/VC/GLView.mm
@@ -10,6 +10,7 @@
 
 @implementation GLView
 
+#pragma mark - init & dealloc
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
@@ -24,9 +25,28 @@
         }
         //initialize for opengl
         GLuint frameBuffer, renderBuffer;
-        
+        glGenFramebuffersOES(1, &frameBuffer);//创建frame(帧)缓冲区
+        glGenRenderbuffersOES(1, &renderBuffer);//创建render（渲染）缓冲区
+        //绑定管线
+        glBindFramebufferOES(GL_FRAMEBUFFER_OES, frameBuffer);
+        glBindRenderbufferOES(GL_RENDERBUFFER_OES, renderBuffer);
+        //分配存储空间
+        [self.glContext renderbufferStorage:GL_RENDERBUFFER_OES fromDrawable:eaglLayer];
+        //绑定 frame 和 render
+        glFramebufferRenderbufferOES(GL_FRAMEBUFFER_OES, GL_COLOR_ATTACHMENT0_OES, GL_RENDERBUFFER_OES, renderBuffer);
+        //构建坐标系统
+        glViewport(0, 0, CGRectGetWidth(frame), CGRectGetHeight(frame));
+        [self drawView];
     }
     return self;
+}
+
+- (void)dealloc
+{
+    if ([EAGLContext currentContext] == self.glContext) {
+        [EAGLContext setCurrentContext:nil];
+    }
+    self.glContext = nil;
 }
 
 /*
@@ -43,5 +63,18 @@
 {
     return [CAEAGLLayer class];
 }
+
+
+#pragma mark - draw
+//这里相当于原ios里的drawRect, 但更提升一层性能（缓冲操作）
+- (void)drawView
+{
+    glClearColor(0.5f, 0.5f, 0.5f, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+    //推送到屏幕
+    [self.glContext presentRenderbuffer:GL_RENDERBUFFER_OES];
+    
+}
+
 
 @end
